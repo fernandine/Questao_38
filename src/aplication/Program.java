f51765e246bd7029fb4bf7ce2b17e8be51cc7437
Jean Fernandine <jfernandine@gmail.com>
2022-04-08 20:18:38 -0300

    correcao data nascimento

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,7 @@ package aplication;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import entity.Pessoa;
 
@@ -10,10 +11,10 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date nascimento = sdf.parse("10/12/1950");
+		Pessoa pessoa = new Pessoa("Joao", nascimento, 1.68);
 		
-		Pessoa pessoa = new Pessoa("Joao", sdf.parse("10/12/1950"), 1.68);
-		
-		System.out.print( pessoa.getNome() + " nasceu em " + sdf.parse("10/12/1950") + " e sua idade é de " + pessoa.CalcularIdade() + " anos. ");
+		System.out.print( pessoa.getNome() + " nasceu em " + sdf.format(nascimento) + " e sua idade é de " + pessoa.CalcularIdade(0) + " anos. ");
 		
 		
 		
--- src/entity/Pessoa.java
+++ src/entity/Pessoa.java
@@ -1,19 +1,26 @@
 package entity;
 
+import java.util.Calendar;
 import java.util.Date;
 
+/*
+ * Crie uma classe para representar uma Pessoa com os atributos privados de nome, data de nascimento e altura. 
+ * Crie os métodos públicos necessários para getters e setters e também um método para imprimir todos dados de uma pessoa. 
+ * Crie um método para calcular a idade da pessoa.
+ */
+
 public class Pessoa {
-	
+
 	private String nome;
-	private Date data;
+	private Date nascimento;
 	private double altura;
-	
+
 	public Pessoa() {
 	}
 
 	public Pessoa(String nome, Date data, double altura) {
 		this.nome = nome;
-		this.data = data;
+		this.nascimento = data;
 		this.altura = altura;
 	}
 
@@ -25,12 +32,12 @@ public class Pessoa {
 		this.nome = nome;
 	}
 
-	public Date getData() {
-		return data;
+	public Date getNascimento() {
+		return nascimento;
 	}
 
-	public void setData(Date data) {
-		this.data = data;
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
 	}
 
 	public double getAltura() {
@@ -40,11 +47,24 @@ public class Pessoa {
 	public void setAltura(double altura) {
 		this.altura = altura;
 	}
-	
 
-	public Date CalcularIdade() {
-	Date idade = new Date();
+	public int CalcularIdade(int idade) {
+		
+		Calendar dataNascimento = Calendar.getInstance();
+		dataNascimento.setTime(nascimento);
+		Calendar hoje = Calendar.getInstance();
+
+		idade = hoje.get(Calendar.YEAR) - dataNascimento.get(Calendar.YEAR);
+
+		if (hoje.get(Calendar.MONTH) < dataNascimento.get(Calendar.MONTH)) {
+			idade--;
+		} else {
+			if (hoje.get(Calendar.MONTH) == dataNascimento.get(Calendar.MONTH)
+					&& hoje.get(Calendar.DAY_OF_MONTH) < dataNascimento.get(Calendar.DAY_OF_MONTH)) {
+				idade--;
+			}
+		}
+
 		return idade;
 	}
-
 }
